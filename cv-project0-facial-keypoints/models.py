6e5ddae10816e6d5d7acd56993aabfb3516e7beb
rawk <era.rawk@gmail.com>
2019-04-09 22:30:06 +0800

    Finish cv-project0-facial-keypoints

--- cv-project0-facial-keypoints/models.py
+++ cv-project0-facial-keypoints/models.py
@@ -1,5 +1,3 @@
-## TODO: define the convolutional neural network architecture
-
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
@@ -7,32 +5,35 @@ import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
+is_debug = False
+
 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        
+
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool2 = nn.MaxPool2d(4, stride=4)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)   # 128*28*28
-        self.pool3 = nn.MaxPool2d(4, stride=4)    # 128*7*7
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)  # 128*28*28
+        self.pool3 = nn.MaxPool2d(2)  # 128*7*7
+        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.pool4 = nn.MaxPool2d(2)
         self.drop1 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(128*7*7, 136)
-        
+        self.fc1 = nn.Linear(128 * 7 * 7, 136)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
 
-        
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
@@ -40,10 +41,14 @@ class Net(nn.Module):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool4(F.relu(self.conv4(x)))
+        if is_debug:
+            print('pool4.out_shape:', x.shape)
         x = x.view(x.size(0), -1)
         x = self.drop1(x)
         x = self.fc1(x)
-
+        if is_debug:
+            print('fc1.out_shape:', x.shape)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
 

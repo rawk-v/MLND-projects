019613e588edc98ec16460d2727f56fbc4421f06
rawk <era.rawk@gmail.com>
2019-04-21 22:46:47 +0800

    Using output of previous time step as current input

--- cv-project1-image-caption/model.py
+++ cv-project1-image-caption/model.py
@@ -34,9 +34,10 @@ class DecoderRNN(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, features, captions):
-#         print("DecoderRNN forward features.shape:", features.shape)
-#         print("DecoderRNN forward captions.shape:", captions.shape)
+        # print("DecoderRNN forward features.shape:", features.shape)
+        # print("DecoderRNN forward captions.shape:", captions.shape)
         features = features.view(features.shape[0], 1, features.shape[1])
+
         h = torch.zeros((self.num_layers, features.shape[0], self.hidden_size), dtype=torch.float32)    # num_layers, batch_size, hidden_size
         c = torch.zeros((self.num_layers, features.shape[0], self.hidden_size), dtype=torch.float32)
 
@@ -46,35 +47,25 @@ class DecoderRNN(nn.Module):
         softmax_outputs = torch.zeros((features.shape[0], captions.shape[1], self.vocab_size))     # batch_size, n_steps, vocab_size
         softmax_outputs = softmax_outputs.cuda()
 
-        lstm_output, hc = self.lstm(features, (h, c))
-        fc1_output = self.fc1(lstm_output)
-        softmax_output = self.softmax(fc1_output)   # TODO, save softmax_output
-        top_k = 1
-        softmax_output_topk = softmax_output.topk(top_k)[1].view(softmax_output.shape[0], top_k)
-        embeded_caption = self.embed(softmax_output_topk)
-        # print('captions.shape:', captions.shape)
-        # print('softmax_outputs.shape:', softmax_outputs.shape)
-        # print('fc1_output.shape:', fc1_output.shape)
-        # print('softmax_output.shape:', softmax_output.shape)
-        # print('softmax_output_topk.shape:', softmax_output_topk.shape)
-        # print('embeded_caption.shape:', embeded_caption.shape)
+        previous_lstm_output = None
+        for i in range(captions.shape[1]):
+            embed_input = None
+            if i == 0:
+                embed_input = features
+            else:
+                embed_input = self.embed(previous_lstm_output)
+            # print('embed_input.shape:', embed_input.shape)
+            lstm_output, (h, c) = self.lstm(embed_input, (h, c))
+            fc1_output = self.fc1(lstm_output)
+            softmax_output = self.softmax(fc1_output)
+            top_k = 1
+            soft_output_topk = softmax_output.topk(top_k)[1].view(softmax_output.shape[0], top_k)
+            # print('soft_output_topk.shape:', soft_output_topk.shape)
+            softmax_outputs[:, i] = softmax_output[:, 0]
 
-        softmax_outputs[:, 0, :] = softmax_output[:, 0, :]
+            previous_lstm_output = soft_output_topk
 
-        for batch in range(features.shape[0]):
-            for step in range(captions.shape[1]-1):
-                # print('embeded_caption.shape:', embeded_caption.shape, ', step:', step)
-                lstm_output, (h[:, batch:batch+1, :], c[:, batch:batch+1, :]) = self.lstm(embeded_caption[batch:batch+1], (h[:, batch:batch+1, :].clone(), c[:, batch:batch+1, :].clone()))
-                fc1_output = self.fc1(lstm_output)
-                softmax_output = self.softmax(fc1_output)   # TODO, save softmax_output
-                # print('softmax_output.shape:', softmax_output.shape)
-                # print(softmax_output.topk(1))
-                softmax_output_topk = softmax_output.topk(1)[1].view(1, -1)
-                embeded_caption[batch:batch+1] = self.embed(softmax_output_topk)
-
-                softmax_outputs[batch, step] = softmax_output[0, 0]
-
-        print("DecoderRNN forward softmax_outputs.shape:", softmax_outputs.shape)
+        # print("DecoderRNN forward softmax_outputs.shape:", softmax_outputs.shape)
         return softmax_outputs
 
     def sample(self, inputs, states=None, max_len=20):
